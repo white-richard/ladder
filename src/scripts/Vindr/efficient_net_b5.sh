48f6860c62aec7e6e698e9783908a2a8c6bf0b11
white-richard <rich.white285@yahoo.com>
2026-02-07 15:21:47 -0800

    resetting

--- src/scripts/Vindr/efficient_net_b5.sh
+++ src/scripts/Vindr/efficient_net_b5.sh
@@ -1,8 +1,8 @@
 # Step 1: Create blackbox model using EfficientNet B5 architecture on VinDr dataset
 python ./src/codebase/train_classifier_Mammo.py \
   --data-dir 'data/Vindr/vindr-mammo-a-large-scale-benchmark-dataset-for-computer-aided-detection-and-diagnosis-in-full-field-digital-mammography-1.0.0' \
-  --img-dir 'External/Vindr/vindr-mammo-a-large-scale-benchmark-dataset-for-computer-aided-detection-and-diagnosis-in-full-field-digital-mammography-1.0.0/images_png' \
-  --csv-file 'External/Vindr/vindr-mammo-a-large-scale-benchmark-dataset-for-computer-aided-detection-and-diagnosis-in-full-field-digital-mammography-1.0.0/vindr_detection_v1_folds_abnormal.csv' \
+  --img-dir 'images_png' \
+  --csv-file 'vindr_detection_v1_folds_abnormal.csv' \
   --dataset 'ViNDr' --arch 'tf_efficientnet_b5_ns-detect' --epochs 20 --batch-size 8 --num-workers 0 \
   --print-freq 10000 --log-freq 500 --running-interactive 'n' \
   --lr 5.0e-5 --weighted-BCE 'y' --balanced-dataloader 'n'  --n_folds 1  --label "abnormal" \
@@ -10,25 +10,38 @@ python ./src/codebase/train_classifier_Mammo.py \
   --checkpoints="out/ViNDr/fold0" \
   --output_path="out/ViNDr/fold0"
 
+echo "=============save_img_reps==================="
+
 
 # Step 2: Save Image Reps EN B5
 python ./src/codebase/save_img_reps.py \
   --seed=0 \
   --dataset="VinDr" \
   --classifier="efficientnet-b5" \
-  --classifier_check_pt="out/ViNDr/fold0/efficientnetb5_seed_10_fold0_best_aucroc_ver084.pth" \
+  --classifier_check_pt="out/ViNDr/fold{}/efficientnetb5_seed_10_fold0_best_aucroc_ver084.pth" \
   --flattening-type="adaptive" \
   --clip_vision_encoder="tf_efficientnet_b5_ns-detect" \
-  --clip_check_pt "model_weights/mammoClip-b5-model-best-epoch-7.tar" \
+  --clip_check_pt "/home/richw/.code/hyp-mammo/repos/ladder/model_weights/mammoClip-b5-model-best-epoch-7.tar" \
   --data_dir="data/Vindr/vindr-mammo-a-large-scale-benchmark-dataset-for-computer-aided-detection-and-diagnosis-in-full-field-digital-mammography-1.0.0" \
   --save_path="out/ViNDr/fold{}" \
-  --tokenizers="$(pwd)/src/codebase/outputs/huggingface/tokenizers" \
-  --cache_dir="$(pwd)/src/codebase/outputs/huggingface/models" \
-  --eval-only
+  --tokenizers="$HOME/.cache/huggingface/tokenizers" \
+  --cache_dir="$HOME/.cache/huggingface/models"
 
+echo "============save_text_reps===================="
 
 # Step 3: Save Text Reps EN B5 valid
-# Same as RSNA
+python ./src/codebase/save_text_reps.py \
+  --seed=0 \
+  --dataset="VinDr" \
+  --clip_vision_encoder="tf_efficientnet_b5_ns-detect" \
+  --clip_check_pt="/home/richw/.code/hyp-mammo/repos/ladder/model_weights/mammoClip-b5-model-best-epoch-7.tar" \
+  --csv="/home/richw/.code/hyp-mammo/repos/ladder/mammo_rad_report.csv" \
+  --save_path="out/ViNDr/fold{}" \
+  --tokenizers="$HOME/.cache/huggingface/tokenizers" \
+  --cache_dir="$HOME/.cache/huggingface/models"
+
+
+echo "===============learn_aligner================="
 
 # Step 4: Train aligner
 python ./src/codebase/learn_aligner.py \
@@ -39,6 +52,7 @@ python ./src/codebase/learn_aligner.py \
   --clf_reps_path="out/ViNDr/fold{0}/clip_img_encoder_tf_efficientnet_b5_ns-detect/{1}_classifier_embeddings.npy" \
   --clip_reps_path="out/ViNDr/fold{0}/clip_img_encoder_tf_efficientnet_b5_ns-detect/{1}_clip_embeddings.npy"
 
+echo "==============discover_error_slices=================="
 
 # Step 5: Retrieving Sentences Indicative of Biases valid
 python ./src/codebase/discover_error_slices.py \
@@ -48,10 +62,11 @@ python ./src/codebase/discover_error_slices.py \
   --save_path="out/ViNDr/fold{}/clip_img_encoder_tf_efficientnet_b5_ns-detect" \
   --clf_results_csv="out/ViNDr/fold{}/clip_img_encoder_tf_efficientnet_b5_ns-detect/valid_additional_info.csv" \
   --clf_image_emb_path="out/ViNDr/fold{}/clip_img_encoder_tf_efficientnet_b5_ns-detect/valid_classifier_embeddings.npy" \
-  --language_emb_path="<save_path for report-embeddings>/sent_emb_word_ge_3.npy" \
-  --sent_path="<save_path for reports>/sentences_word_ge_3.pkl" \
+  --language_emb_path="out/ViNDr/fold{}/clip_img_encoder_tf_efficientnet_b5_ns-detect/sent_emb_word_ge_3.npy" \
+  --sent_path="out/ViNDr/fold{}/clip_img_encoder_tf_efficientnet_b5_ns-detect/sentences_word_ge_3.pkl" \
   --aligner_path="out/ViNDr/fold{}/clip_img_encoder_tf_efficientnet_b5_ns-detect/aligner_30.pth"
 
+echo "=============validate_error_slices_w_LLM==================="
 
 # Step 6: Discovering Error Slices via LLM  valid
 python ./src/codebase/validate_error_slices_w_LLM.py \
@@ -60,15 +75,16 @@ python ./src/codebase/validate_error_slices_w_LLM.py \
   --class_label="abnormal" \
   --clip_vision_encoder="tf_efficientnet_b5_ns-detect" \
   --key="" \
-  --clip_check_pt="out/RSNA/fold0/b5-model-best-epoch-7.tar" \
+  --clip_check_pt="/home/richw/.code/hyp-mammo/repos/ladder/model_weights/mammoClip-b5-model-best-epoch-7.tar" \
   --top50-err-text="out/ViNDr/fold{}/clip_img_encoder_tf_efficientnet_b5_ns-detect/abnormal_error_top_100_sent_diff_emb.txt" \
   --save_path="out/ViNDr/fold{}/clip_img_encoder_tf_efficientnet_b5_ns-detect" \
   --clf_results_csv="out/ViNDr/fold{}/clip_img_encoder_tf_efficientnet_b5_ns-detect/{}_additional_info.csv" \
   --clf_image_emb_path="out/ViNDr/fold{}/clip_img_encoder_tf_efficientnet_b5_ns-detect/{}_classifier_embeddings.npy" \
   --aligner_path="out/ViNDr/fold{}/clip_img_encoder_tf_efficientnet_b5_ns-detect/aligner_30.pth" \
-  --tokenizers="" \
-  --cache_dir=""
+  --tokenizers="$HOME/.cache/huggingface/tokenizers" \
+  --cache_dir="$HOME/.cache/huggingface/models"
 
+echo "=============mitigate_error_slices==================="
 
 # Step 7: Mitigate error slices valid
 python ./src/codebase/mitigate_error_slices.py \
