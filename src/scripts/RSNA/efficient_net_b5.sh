430f1fb00cb71958446bf1d031f9b1d11082d258
white-richard <rich.white285@yahoo.com>
2026-02-07 19:27:34 -0800

    updated rsna run script

--- src/scripts/RSNA/efficient_net_b5.sh
+++ src/scripts/RSNA/efficient_net_b5.sh
@@ -1,6 +1,5 @@
-# Step 1: Create blackbox model using EfficientNet B5 architecture on RSNA dataset
 python ./src/codebase/train_classifier_Mammo.py \
-  --data-dir '
+  --data-dir 'data/RSNA_Breast_Imaging/Dataset' \
   --img-dir 'RSNA_Cancer_Detection/train_images_png' \
   --csv-file 'RSNA_Cancer_Detection/rsna_w_upmc_concepts_breast_clip.csv' --start-fold 0 --n_folds 1 \
   --dataset 'RSNA' --arch 'tf_efficientnet_b5_ns-detect' --epochs 9 --batch-size 6 --num-workers 0 \
@@ -9,12 +8,9 @@ python ./src/codebase/train_classifier_Mammo.py \
   --tensorboard-path="out/RSNA/fold0" \
   --checkpoints="out/RSNA/fold0" \
   --output_path="out/RSNA/fold0" \
-  --label "cancer"
-
-
-
-# Step 2: Save Image Reps EN B5 valid
-python ./src/codebase/save_img_reps.py \
+  --label "cancer" \
+&& echo "=============save_img_reps===================" \
+&& python ./src/codebase/save_img_reps.py \
   --seed=0 \
   --dataset="RSNA" \
   --classifier="efficientnet-b5" \
@@ -22,38 +18,30 @@ python ./src/codebase/save_img_reps.py \
   --flattening-type="adaptive" \
   --clip_vision_encoder="tf_efficientnet_b5_ns-detect" \
   --clip_check_pt "out/RSNA/fold0/b5-model-best-epoch-7.tar" \
-  --data_dir="ction" \
+  --data_dir="data/RSNA_Breast_Imaging/Dataset/RSNA_Cancer_Detection" \
   --save_path="out/RSNA/fold{}/aucroc0.89" \
-  --tokenizers="/restricted/projectnb/batmanlab/shawn24/PhD" \
-  --cache_dir="/restricted/projectnb/batmanlab/shawn24/PhD"
-
-
-
-# Step 3: Save Text Reps EN B5 valid
-python ./src/codebase/save_text_reps.py \
+  --tokenizers="$HOME/.cache/huggingface/tokenizers" \
+  --cache_dir="$HOME/.cache/huggingface/models" \
+&& echo "============save_text_reps====================" \
+&& python ./src/codebase/save_text_reps.py \
   --seed=0 \
   --dataset="RSNA" \
   --clip_vision_encoder="tf_efficientnet_b5_ns-detect" \
   --clip_check_pt="out/RSNA/fold0/b5-model-best-epoch-7.tar" \
   --csv="<csv file containing report texts>" \
   --save_path="out/RSNA/fold{}/aucroc0.89" \
-  --tokenizers="/restricted/projectnb/batmanlab/shawn24/PhD" \
-  --cache_dir="/restricted/projectnb/batmanlab/shawn24/PhD"
-
-
-
-# Step 4: Train aligner with valid
-python ./src/codebase/learn_aligner.py \
+  --tokenizers="$HOME/.cache/huggingface/tokenizers" \
+  --cache_dir="$HOME/.cache/huggingface/models" \
+&& echo "===============learn_aligner=================" \
+&& python ./src/codebase/learn_aligner.py \
   --seed=0 \
   --epochs=30 \
   --dataset="RSNA" \
   --save_path="out/RSNA/fold{0}/aucroc0.89/clip_img_encoder_tf_efficientnet_b5_ns-detect" \
   --clf_reps_path="out/RSNA/fold{0}/aucroc0.89/clip_img_encoder_tf_efficientnet_b5_ns-detect/{1}_classifier_embeddings.npy" \
-  --clip_reps_path="out/RSNA/fold{0}/aucroc0.89/clip_img_encoder_tf_efficientnet_b5_ns-detect/{1}_clip_embeddings.npy"
-
-
-# Step 5: Retrieving Sentences Indicative of Biases valid
-python ./src/codebase/discover_error_slices.py \
+  --clip_reps_path="out/RSNA/fold{0}/aucroc0.89/clip_img_encoder_tf_efficientnet_b5_ns-detect/{1}_clip_embeddings.npy" \
+&& echo "==============discover_error_slices==================" \
+&& python ./src/codebase/discover_error_slices.py \
   --seed=0 \
   --topKsent=100 \
   --dataset="RSNA" \
@@ -62,12 +50,9 @@ python ./src/codebase/discover_error_slices.py \
   --clf_image_emb_path="out/RSNA/fold{}/aucroc0.89/clip_img_encoder_tf_efficientnet_b5_ns-detect/valid_classifier_embeddings.npy" \
   --language_emb_path="<save_path for report-embeddings>/sent_emb_word_ge_3.npy" \
   --sent_path="<save_path for reports>/sentences_word_ge_3.pkl" \
-  --aligner_path="out/RSNA/fold{}/aucroc0.89/clip_img_encoder_tf_efficientnet_b5_ns-detect/aligner_30.pth"
-
-
-
-# Step 6: Discovering Error Slices via LLM  valid
-python ./src/codebase/validate_error_slices_w_LLM.py \
+  --aligner_path="out/RSNA/fold{}/aucroc0.89/clip_img_encoder_tf_efficientnet_b5_ns-detect/aligner_30.pth" \
+&& echo "=============validate_error_slices_w_LLM===================" \
+&& python ./src/codebase/validate_error_slices_w_LLM.py \
   --seed=0 \
   --dataset="RSNA" \
   --class_label="cancer" \
@@ -79,12 +64,10 @@ python ./src/codebase/validate_error_slices_w_LLM.py \
   --clf_results_csv="out/RSNA/fold{}/aucroc0.89/clip_img_encoder_tf_efficientnet_b5_ns-detect/{}_additional_info.csv" \
   --clf_image_emb_path="out/RSNA/fold{}/aucroc0.89/clip_img_encoder_tf_efficientnet_b5_ns-detect/{}_classifier_embeddings.npy" \
   --aligner_path="out/RSNA/fold{}/aucroc0.89/clip_img_encoder_tf_efficientnet_b5_ns-detect/aligner_30.pth" \
-  --tokenizers="/restricted/projectnb/batmanlab/shawn24/PhD" \
-  --cache_dir="/restricted/projectnb/batmanlab/shawn24/PhD"
-
-
-# Step 7: Mitigate error slices valid
-python ./src/codebase/mitigate_error_slices.py \
+  --tokenizers="$HOME/.cache/huggingface/tokenizers" \
+  --cache_dir="$HOME/.cache/huggingface/models" \
+&& echo "=============mitigate_error_slices===================" \
+&& python ./src/codebase/mitigate_error_slices.py \
   --seed=0 \
   --epochs=30 \
   --n=75 \
