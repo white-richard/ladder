8482e0b00693eec5caea652de0290de0e7ba63bd
white-richard <rich.white285@yahoo.com>
2026-02-09 23:40:44 -0800

    formatting

--- src/scripts/Vindr/efficient_net_b5.sh
+++ src/scripts/Vindr/efficient_net_b5.sh
@@ -1,4 +1,3 @@
-# Step 1: Create blackbox model using EfficientNet B5 architecture on VinDr dataset
 python ./src/codebase/train_classifier_Mammo.py \
   --data-dir 'data/Vindr/vindr-mammo-a-large-scale-benchmark-dataset-for-computer-aided-detection-and-diagnosis-in-full-field-digital-mammography-1.0.0' \
   --img-dir 'images_png' \
@@ -8,13 +7,9 @@ python ./src/codebase/train_classifier_Mammo.py \
   --lr 5.0e-5 --weighted-BCE 'y' --balanced-dataloader 'n'  --n_folds 1  --label "abnormal" \
   --tensorboard-path="out/ViNDr/fold0" \
   --checkpoints="out/ViNDr/fold0" \
-  --output_path="out/ViNDr/fold0"
-
-echo "=============save_img_reps==================="
-
-
-# Step 2: Save Image Reps EN B5
-python ./src/codebase/save_img_reps.py \
+  --output_path="out/ViNDr/fold0" \
+&& echo "=============save_img_reps===================" \
+&& python ./src/codebase/save_img_reps.py \
   --seed=0 \
   --dataset="VinDr" \
   --classifier="efficientnet-b5" \
@@ -25,12 +20,9 @@ python ./src/codebase/save_img_reps.py \
   --data_dir="data/Vindr/vindr-mammo-a-large-scale-benchmark-dataset-for-computer-aided-detection-and-diagnosis-in-full-field-digital-mammography-1.0.0" \
   --save_path="out/ViNDr/fold{}" \
   --tokenizers="$HOME/.cache/huggingface/tokenizers" \
-  --cache_dir="$HOME/.cache/huggingface/models"
-
-echo "============save_text_reps===================="
-
-# Step 3: Save Text Reps EN B5 valid
-python ./src/codebase/save_text_reps.py \
+  --cache_dir="$HOME/.cache/huggingface/models" \
+&& echo "============save_text_reps====================" \
+&& python ./src/codebase/save_text_reps.py \
   --seed=0 \
   --dataset="VinDr" \
   --clip_vision_encoder="tf_efficientnet_b5_ns-detect" \
@@ -38,24 +30,17 @@ python ./src/codebase/save_text_reps.py \
   --csv="/home/richw/.code/hyp-mammo/repos/ladder/mammo_rad_report.csv" \
   --save_path="out/ViNDr/fold{}" \
   --tokenizers="$HOME/.cache/huggingface/tokenizers" \
-  --cache_dir="$HOME/.cache/huggingface/models"
-
-
-echo "===============learn_aligner================="
-
-# Step 4: Train aligner
-python ./src/codebase/learn_aligner.py \
+  --cache_dir="$HOME/.cache/huggingface/models" \
+&& echo "===============learn_aligner=================" \
+&& python ./src/codebase/learn_aligner.py \
   --seed=0 \
   --epochs=30 \
   --dataset="VinDr" \
   --save_path="out/ViNDr/fold{0}/clip_img_encoder_tf_efficientnet_b5_ns-detect" \
   --clf_reps_path="out/ViNDr/fold{0}/clip_img_encoder_tf_efficientnet_b5_ns-detect/{1}_classifier_embeddings.npy" \
-  --clip_reps_path="out/ViNDr/fold{0}/clip_img_encoder_tf_efficientnet_b5_ns-detect/{1}_clip_embeddings.npy"
-
-echo "==============discover_error_slices=================="
-
-# Step 5: Retrieving Sentences Indicative of Biases valid
-python ./src/codebase/discover_error_slices.py \
+  --clip_reps_path="out/ViNDr/fold{0}/clip_img_encoder_tf_efficientnet_b5_ns-detect/{1}_clip_embeddings.npy" \
+&& echo "==============discover_error_slices==================" \
+&& python ./src/codebase/discover_error_slices.py \
   --seed=0 \
   --topKsent=100 \
   --dataset="ViNDr" \
@@ -64,12 +49,9 @@ python ./src/codebase/discover_error_slices.py \
   --clf_image_emb_path="out/ViNDr/fold{}/clip_img_encoder_tf_efficientnet_b5_ns-detect/valid_classifier_embeddings.npy" \
   --language_emb_path="out/ViNDr/fold{}/clip_img_encoder_tf_efficientnet_b5_ns-detect/sent_emb_word_ge_3.npy" \
   --sent_path="out/ViNDr/fold{}/clip_img_encoder_tf_efficientnet_b5_ns-detect/sentences_word_ge_3.pkl" \
-  --aligner_path="out/ViNDr/fold{}/clip_img_encoder_tf_efficientnet_b5_ns-detect/aligner_30.pth"
-
-echo "=============validate_error_slices_w_LLM==================="
-
-# Step 6: Discovering Error Slices via LLM  valid
-python ./src/codebase/validate_error_slices_w_LLM.py \
+  --aligner_path="out/ViNDr/fold{}/clip_img_encoder_tf_efficientnet_b5_ns-detect/aligner_30.pth" \
+&& echo "=============validate_error_slices_w_LLM===================" \
+&& python ./src/codebase/validate_error_slices_w_LLM.py \
   --seed=0 \
   --dataset="ViNDr" \
   --class_label="abnormal" \
@@ -82,12 +64,9 @@ python ./src/codebase/validate_error_slices_w_LLM.py \
   --clf_image_emb_path="out/ViNDr/fold{}/clip_img_encoder_tf_efficientnet_b5_ns-detect/{}_classifier_embeddings.npy" \
   --aligner_path="out/ViNDr/fold{}/clip_img_encoder_tf_efficientnet_b5_ns-detect/aligner_30.pth" \
   --tokenizers="$HOME/.cache/huggingface/tokenizers" \
-  --cache_dir="$HOME/.cache/huggingface/models"
-
-echo "=============mitigate_error_slices==================="
-
-# Step 7: Mitigate error slices valid
-python ./src/codebase/mitigate_error_slices.py \
+  --cache_dir="$HOME/.cache/huggingface/models" \
+&& echo "=============mitigate_error_slices===================" \
+&& python ./src/codebase/mitigate_error_slices.py \
   --seed=0 \
   --epochs=30 \
   --n=75 \
